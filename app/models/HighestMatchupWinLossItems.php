5883be217b0add34a99dfe72d9cfb47133c86161
kyle-young <readme.py@gmail.com>
2015-08-17 21:18:20 -0700

    Added getSource() and relationshps between HighestMatchupWinLossItems, HighestMatchupWinLossSkills, HighestOverallWinLossItems, HighestOverallWinLossSkills, to ItemSet

--- app/models/HighestMatchupWinLossItems.php
+++ app/models/HighestMatchupWinLossItems.php
@@ -13,4 +13,19 @@ class HighestMatchupWinLossItems extends \Phalcon\Mvc\Model
     * @var integer
     */
    public $item_set_id;
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'highest_matchup_win_loss_items';
+    }
+
+    public function initialize()
+    {
+        $this->hasMany("item_set_id", "ItemSet", "item_set_id");
+    }
 }
--- app/models/HighestOverallWinLossSkills.php
+++ app/models/HighestOverallWinLossSkills.php
@@ -13,4 +13,19 @@ class HighestWinLossSkills extends \Phalcon\Mvc\Model
     * @var integer
     */
    public $item_set_id;
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'highest_overall_win_loss_skills';
+    }
+
+    public function initialize()
+    {
+        $this->hasMany("item_set_id", "ItemSet", "item_set_id");
+    }
 }
