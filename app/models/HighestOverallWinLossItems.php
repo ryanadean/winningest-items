b6b6ebd5ed54993a90d2ca657b00ebb9ae78ed18
kyle-young <readme.py@gmail.com>
2015-08-17 22:07:23 -0700

    Added getter and updater skeletons for highest item/skill set models

--- app/models/HighestOverallWinLossItems.php
+++ app/models/HighestOverallWinLossItems.php
@@ -2,17 +2,49 @@
 
 class HighestWinLossItems extends \Phalcon\Mvc\Model
 {
-   /**
-    *
-    * @var integer
-    */
-   public $champion_id; 
+    /**
+     *
+     * @var integer
+     */
+    public $champion_id; 
 
-   /**
-    *
-    * @var integer
-    */
-   public $item_set_id;
+    /**
+     *
+     * @var integer
+     */
+    public $item_set_id;
+
+    /**
+     * Update all champions' highest w/l matchup item sets
+     *
+     */
+    public function updateAll()
+    {
+    }
+
+    /**
+     * Update specific champion's highest w/l matchup item set
+     *
+     */
+    public function updateSpecific()
+    {
+    }
+
+    /**
+     * Get item set for specific champion
+     *
+     */ 
+    public function getSpecific()
+    {
+    }
+
+    /**
+     * Get item sets for all champions
+     *
+     */ 
+    public function getAll()
+    {
+    }
 
     /**
      * Returns table name mapped in the model.
@@ -26,6 +58,6 @@ class HighestWinLossItems extends \Phalcon\Mvc\Model
 
     public function initialize()
     {
-        $this->hasMany("item_set_id", "ItemSet", "item_set_id");
+        $this->hasOne("item_set_id", "ItemSet", "item_set_id");
     }
 }
--- app/models/HighestOverallWinLossSkills.php
+++ app/models/HighestOverallWinLossSkills.php
@@ -2,17 +2,49 @@
 
 class HighestWinLossSkills extends \Phalcon\Mvc\Model
 {
-   /**
-    *
-    * @var integer
-    */
-   public $champion_id; 
+    /**
+     *
+     * @var integer
+     */
+    public $champion_id; 
 
-   /**
-    *
-    * @var integer
-    */
-   public $item_set_id;
+    /**
+     *
+     * @var integer
+     */
+    public $item_set_id;
+
+    /**
+     * Update all champions' highest w/l matchup skill sets
+     *
+     */
+    public function updateAll()
+    {
+    }
+
+    /**
+     * Update specific champion's highest w/l matchup skill set
+     *
+     */
+    public function updateSpecific()
+    {
+    }
+
+    /**
+     * Get skill set for specific champion
+     *
+     */ 
+    public function getSpecific()
+    {
+    }
+
+    /**
+     * Get skill sets for all champions
+     *
+     */ 
+    public function getAll()
+    {
+    }
 
     /**
      * Returns table name mapped in the model.
@@ -26,6 +58,6 @@ class HighestWinLossSkills extends \Phalcon\Mvc\Model
 
     public function initialize()
     {
-        $this->hasMany("item_set_id", "ItemSet", "item_set_id");
+        $this->hasOne("item_set_id", "ItemSet", "item_set_id");
     }
 }
