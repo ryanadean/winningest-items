44091e608091c4566bdca991c0b2e9452770a7b0
kyle-young <readme.py@gmail.com>
2015-08-18 17:38:11 -0700

    Skeleton for controller to handle specific champions

---
+++ app/controllers/ChampionController.php
@@ -0,0 +1,16 @@
+<?php
+
+class ChampionController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+    }
+
+    public function championAction($champion_name)
+    {
+        print($champion_name);
+    }    
+    
+}
+
