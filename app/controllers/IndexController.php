9161a76312c411f0320659f53b809041a53a6f0b
kyle-young <readme.py@gmail.com>
2015-08-19 20:31:50 -0700

    Transferred champion list maker to get list from mysql instead of local array

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -4,8 +4,15 @@ class IndexController extends ControllerBase
 {
     public function indexAction()
     {
-        $champions = array('Ahri', 'Aatrox', 'Sejuani');
-        //$champions = Champion::find();
+        set_include_path(dirname(__FILE__) . '/../config/');
+        // Connect to database
+        $config = new Phalcon\Config\Adapter\Php("champion.php");
+        $connection = new \Phalcon\Db\Adapter\Pdo\Mysql($config->database->toArray());
+        $champions = array();
+        $results = $connection->fetchAll("SELECT * FROM champion");
+        foreach($results as $result){
+            array_push($champions, $result["champion_name"]);
+        }
         $this->view->setVar('champions',$champions);
     }
 }
