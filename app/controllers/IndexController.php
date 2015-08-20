a7f04fd281cb363bcf56d903f42d779789104415
kyle-young <readme.py@gmail.com>
2015-08-20 11:09:31 -0700

    Connect to db via models instead of directly to make champion grid

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -4,19 +4,20 @@ class IndexController extends ControllerBase
 {
     public function indexAction()
     {
-        set_include_path(dirname(__FILE__) . '/../config/');
-        // Connect to database
-        $config = new Phalcon\Config\Adapter\Php("champion.php");
-        $connection = new \Phalcon\Db\Adapter\Pdo\Mysql($config->database->toArray());
+        // Get all champions in database, don't need champion ids
+        $results = Champion::find(
+            array(
+                "columns" => "champion_name",
+                "order" => "champion_name"
+            )
+        );
 
         // Make array for champion list
         $champions = array();
-        // Get all champions in database
-        $results = $connection->fetchAll("SELECT * FROM champion ORDER BY champion_name;");
 
         // Push each champion onto array
         foreach($results as $result){
-            array_push($champions, $result["champion_name"]);
+            array_push($champions, $result->champion_name);
         }
         $this->view->setVar('champions',$champions);
     }
