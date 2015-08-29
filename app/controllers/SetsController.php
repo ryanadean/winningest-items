f72a5b622e054b8f95135ba0016d5c65153b70dc
kyle-young <readme.py@gmail.com>
2015-08-28 21:52:39 -0700

    Forgot SQL is different from PHQL, removed SQL

--- app/controllers/SetsController.php
+++ app/controllers/SetsController.php
@@ -9,13 +9,7 @@ class SetsController extends ControllerBase
     // Updates Item and Skill sets for overall option
     public function updateChampionOverallAction($champion_id)
     {
-        $overall_phql = "
-SELECT match_participant.id, match_participant.player_id, champion_id, summoner_1, summoner_2, team_id, item0, item1, item2, item3, item4, item5, item6, winner
-FROM match_participant
-INNER JOIN participant_stats ON match_participant.player_id = participant_stats.player_id
-AND match_participant.id = participant_stats.id
-WHERE champion_id = :champion_id
-AND winner = 1";
+        $overall_phql = "";
         print("Executing query");
         $champions = $manager->executeQuery($overall_phql, array("champion_id" => $champion_id));
         
@@ -50,26 +44,7 @@ AND winner = 1";
     // Updates Item and Skill sets for matchups
     public function updateChampionVSAction($champion_id, $vs_id)
     {
-        $vs_phql = "
-SELECT t1.* FROM
-(
-SELECT match_participant.id, match_participant.player_id, champion_id, summoner_1, summoner_2, team_id, item0, item1, item2, item3, item4, item5, item6, winner
-FROM match_participant
-INNER JOIN participant_stats ON match_participant.player_id = participant_stats.player_id
-AND match_participant.id = participant_stats.id
-WHERE (champion_id = 1 AND winner = 1)
-) t1
-INNER JOIN
-(
-SELECT match_participant.id, match_participant.player_id, champion_id, summoner_1, summoner_2, team_id, item0, item1, item2, item3, item4, item5, item6, winner
-FROM match_participant
-INNER JOIN participant_stats ON match_participant.player_id = participant_stats.player_id
-AND match_participant.id = participant_stats.id
-WHERE (champion_id = 2 AND winner = 0)
-) t2
-ON t1.id = t2.id
-
-WHERE t1.champion_id = :champion_id AND t2.champion_id = :vs_id";
+        $vs_phql = "";
 
         print("Executing query");
         $champions = $manager->executeQuery($vs_phql, array("champion_id" => $champion_id, "vs_id" => $vs_id));
