3727e6bb9ba5060f11819735b60879b70166846a
kyle-young <readme.py@gmail.com>
2015-08-31 01:04:38 -0700

    Added code to analyse what the winningest set is

--- app/controllers/SetsController.php
+++ app/controllers/SetsController.php
@@ -12,23 +12,38 @@ class SetsController extends ControllerBase
         $overall_phql = "";
         print("Executing query");
         $games = $manager->executeQuery($overall_phql, array("champion_id" => $champion_id));
+
+        $all_item_sets = array();
         
         foreach($games as $game)
         {
-            print($game);
+            // Gets item list and sorts
+            $game_items = implode(' ', sort(explode(' ', $game->items)));
+
+            // If item set exists then increment, otherwise add in item set
+            if (array_key_exists($item_array, $all_item_sets))
+            {
+                $all_item_sets['game_items'] += 1;
+            }else
+            {
+                $all_item_sets += array('game_items' => 1);
+            }
         }
 
-        print("Inserting overall item set into CachedData");
+        // Get item set with most wins
+        $winningest_item_set = array_search(max($all_item_sets),$all_item_sets);
+
+        print("Inserting highest overall item set into CachedData");
         $overall_item_set = new CachedData();
         $overall_item_set->save(
             array(
                 "type" => "overall_item_set",
                 "conditionals" => json_encode(array("champion_id" => $champion_id, "vs_id" => "overall")), 
-                "value" => "",
+                "value" => $winningest_item_set,
                 "updated_at" => time()
             )
         );
-
+        /**
         print("Inserting overall skill set into CachedData");
         $overall_skill_set = new CachedData();
         $overall_skill_set->save(
@@ -39,6 +54,7 @@ class SetsController extends ControllerBase
                 "updated_at" => time()
             )
         );
+        **/
     }
 
     // Updates Item and Skill sets for matchups
@@ -49,22 +65,37 @@ class SetsController extends ControllerBase
         print("Executing query");
         $games = $manager->executeQuery($vs_phql, array("champion_id" => $champion_id, "vs_id" => $vs_id));
         
+        $all_item_sets = array();
+        
         foreach($games as $game)
         {
-            print($game);
+            // Gets item list and sorts
+            $game_items = implode(' ', sort(explode(' ', $game->items)));
+
+            // If item set exists then increment, otherwise add in item set
+            if (array_key_exists($item_array, $all_item_sets))
+            {
+                $all_item_sets['game_items'] += 1;
+            }else
+            {
+                $all_item_sets += array('game_items' => 1);
+            }
         }
 
+        // Get item set with most wins
+        $winningest_item_set = array_search(max($all_item_sets),$all_item_sets);
         print("Inserting vs item set into CachedData");
         $vs_item_set = new CachedData();
         $vs_item_set->save(
             array(
                 "type" => "vs_item_set",
                 "conditionals" => json_encode(array("champion_id" => $champion_id, "vs_id" => $vs_id)),
-                "value" => "",
+                "value" => $winningest_item_set,
                 "updated_at" => time()
             )
         );
 
+        /**
         print("Inserting vs skill set into CachedData");
         $vs_skill_set = new CachedData();
         $vs_skill_set->save(
@@ -75,5 +106,6 @@ class SetsController extends ControllerBase
                 "updated_at" => time()
             )
         );
+        **/
     }
 }
